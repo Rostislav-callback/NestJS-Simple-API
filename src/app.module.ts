f4f731ddbb207872afc8958abd8bfe4b3bc979ab
Rostislav-callback <77014059+Rostislav-callback@users.noreply.github.com>
2021-08-19 13:09:20 +0200

    Add files via upload

---
+++ src/app.module.ts
@@ -0,0 +1,31 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+import { TokenModule } from './token/token.module';
+
+import { UserModule } from './user/user.module';
+import { AuthModule } from './auth/auth.module';
+import { MailModule } from './mail/mail.module';
+
+const environment = process.env.NODE_ENV || 'development';
+
+@Module({
+  imports: [
+    UserModule, 
+    AuthModule,
+    ConfigModule.forRoot({
+      envFilePath: `.env.${environment}`,
+      isGlobal: true,
+    }),
+    MongooseModule.forRoot(
+      process.env.MONGODB_WRITE_CONECTION_STRING,
+      {
+        useNewUrlParser: true,
+        useUnifiedTopology:true,
+      },
+    ),
+    MailModule,
+    TokenModule
+  ],
+})
+export class AppModule {}
---
+++ src/user/interfaces/address.interface.ts
@@ -0,0 +1,7 @@
+export interface IAddress {
+    readonly country: string,
+    status: string,
+    readonly city: string,
+    readonly addressLine1: string,
+    readonly addressLine2: string,
+}
